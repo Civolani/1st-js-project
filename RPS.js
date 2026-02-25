781558981b00d1381305c66310f56a1737ca1f8f
Civolani <98224549+Civolani@users.noreply.github.com>
2026-02-25 09:55:01 -0300

    1

---
+++ RPS.js
@@ -0,0 +1,12 @@
+let dropdown = document.querySelectorAll('button')
+
+let weapon = ""
+
+dropdown.forEach(function(selection) {
+    selection.addEventListener('click', function(choice) {
+        if (choice.target.textContent !== "shoot") {weapon = choice.target.textContent
+        } else {
+            
+        }
+    }
+)})
