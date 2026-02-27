09660cf9567321d1566274a50aec2d7038fd82a6
Civolani <98224549+Civolani@users.noreply.github.com>
2026-02-27 09:12:53 -0300

    CPU

--- RPS.js
+++ RPS.js
@@ -1,25 +1,31 @@
 
 let selectedweapon = ""
 
-let weapon = document.querySelectorAll('.weapon')
+let weaponList = document.querySelectorAll('.weapon')
 
+let cpu = document.getElementById('cpu-choice')
+
+let options = ['Paper', 'Scissors', 'Rock']
+let index = 0 
+
+setInterval(function() {
+    cpu.textContent = options[index];
+    index = (index + 1) % 3
+}, 500)
 
 document.addEventListener('click', function (clickedout) {
-    if (clickedout.target.classList === "userchoices") {
-    weapon.forEach(function(btn) {btn.classList.remove('selected')})
+    weaponList.forEach(function(btn) {btn.classList.remove('selected')});
+    if (clickedout.target.classList.contains('weapon')) {
+      clickedout.target.classList.add('selected')
+      selectedweapon = clickedout.target.textContent;
+      console.log(selectedweapon)
     }
+
 })
 
-weapon.forEach(function(choice) {
-    choice.addEventListener('click', function(object) {
-    weapon.forEach(function(btn) {btn.classList.remove('selected')});
-    object.target.classList.add('selected')
-    selectedweapon = object.target.textContent;
-    console.log(selectedweapon)
-})})
 
 document.getElementById('shoot').addEventListener('click', function (deselect) {
-    weapon.forEach(function(btn) {btn.classList.remove('selected')})
+    weaponList.forEach(function(btn) {btn.classList.remove('selected')})
     console.log('Shoot!')
 })
 
