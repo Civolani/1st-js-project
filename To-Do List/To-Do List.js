8218005d1073063e5b88511edd7b6255c400bfa0
Civolani <98224549+Civolani@users.noreply.github.com>
2026-03-09 08:39:59 -0300

    check

--- To-Do List/To-Do List.js
+++ To-Do List/To-Do List.js
@@ -3,12 +3,19 @@ let addbutton = document.getElementById('add-button')
 
 let list = document.getElementById('list')
 
-let newtask = document.createElement('li')
+
+
+
 
 addbutton.addEventListener('click', function() {
 
     let newtask = document.createElement('li')
-    newtask.textContent = document.getElementById('new-item').value
-    list.appendChild(newtask)
+    let taskHTML = '<input type="checkbox" style=margin-right:10px>';
+
+    newtask.innerHTML = taskHTML + document.getElementById('new-item').value
     
+    list.appendChild(newtask)
+
+    document.getElementById('new-task').textContent = ""
+
 })
