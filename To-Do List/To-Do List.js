f92c19e8f97291aa2ff6d0aa1d5ec63d8dca8af5
Civolani <98224549+Civolani@users.noreply.github.com>
2026-03-12 09:06:03 -0300

    .

--- To-Do List/To-Do List.js
+++ To-Do List/To-Do List.js
@@ -5,23 +5,40 @@ let list = document.getElementById('list')
 
 let listcheckbox = document.getElementById('listcheckbox')
 
-
-
-addbutton.addEventListener('click', function() {
+function addtask() {
     if (document.getElementById('new-item').value.trim() !== ""){
     let newtask = document.createElement('li')
     let taskHTML = '<input type="checkbox" id="listcheckbox" style=margin-right:10px>';
+    let removebutton = '<button id="removebutton">━</button>'
 
-    newtask.innerHTML = taskHTML + document.getElementById('new-item').value
+    newtask.innerHTML = taskHTML + document.getElementById('new-item').value + removebutton
     
     list.appendChild(newtask)
 
     document.getElementById('new-item').value = ""
     }
+}
+
+function loadtasks() {
+    let = tasks = []
+    document.querySelectorAll('#list li'),forEach(function(addlistitem) {
+        
+    })
+}
+
+addbutton.addEventListener('click', addtask)
+
+document.getElementById('new-item').addEventListener('keydown', function(ent) {
+    if (ent.key === 'Enter') {addtask()}
 })
 
-list.addEventListener('change', function(check) {
-    if (check.target.type === 'checkbox') {
-        {check.target.parentElement.classList.toggle('checked')}}
+list.addEventListener('click', function(clickedobject) {
+    if (clickedobject.target.type === 'checkbox') {
+        {clickedobject.target.parentElement.classList.toggle('checked')}}
+
+    if (clickedobject.target.id === 'removebutton') {
+        clickedobject.target.parentElement.remove()
+    }
 })
 
+loadtasks()
